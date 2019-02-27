57a4aec1842de1547008f58878770ff73a06f6fd
HVK <vkasikaralar@gmail.com>
2019-02-27 22:40:28 +0300

    Message take fixed. Alarm check, rise and close systems written. V1 finished

--- Medicine_Box/Medicine_Box.ino
+++ Medicine_Box/Medicine_Box.ino
@@ -2,7 +2,7 @@
 // #
 // # H. Volkan Kasikaralar
 // # Medicine Box
-// # 
+// #
 // # 26.02.2019
 // #
 // ##################################################################################
@@ -14,9 +14,23 @@ uint16_t startAddr = 0x0000;            // Ram Start Address
 uint16_t lastAddr;                      // Ram Clock Adress
 uint16_t TimeIsSet = 0xaa55;            // Is Clock Set?
 
+const int buzzer = 12;
+const int buttonMon = 11;
+const int buttonTue = 10;
+const int buttonWed = 9;
+const int buttonThu = 8;
+const int buttonFri = 7;
+const int buttonSat = 6;
+const int buttonSun = 5;
+const int lightSetting1 = 4;
+const int lightSetting2 = 3;
+const int lightSetting3 = 2;
+const int lightControl = A0;
+
 String comMsg = "";                     // Income from HC-05, DEL or ADD
 String dayOfWeek = "";                  // Day of Week
 String clockData = "";                  // Data of clock
+String minData = "";                    // Data of minute
 
 String clocksMon[10];                   // Clocks of Monday
 String clocksTue[10];                   // Clocks of Tuesday
@@ -26,10 +40,23 @@ String clocksFri[10];                   // Clocks of Friday
 String clocksSat[10];                   // Clocks of Saturday
 String clocksSun[10];                   // Clocks of Sunday
 
-int numberOfAlarms[7] = {0, 0, 0, 0, 0, 0, 0};              // Number of alarms set on Week
+String minMon[10];                   // Minute of Monday
+String minTue[10];                   // Minute of Tuesday
+String minWed[10];                   // Minute of Wednesday
+String minThu[10];                   // Minute of Thursday
+String minFri[10];                   // Minute of Friday
+String minSat[10];                   // Minute of Saturday
+String minSun[10];                   // Minute of Sunday
+
+int numberOfAlarms[7] = {0, 0, 0, 0, 0, 0, 0};  // Number of alarms set on Week
+int alarmStatus[7] = {0, 0, 0, 0, 0, 0, 0};  // Number of alarms set on Week
+
+int delayPeriod = 500;
+int buttonState = 0;
 
 boolean stringComplete = false;         // When serial message finished
 boolean exist = false;
+boolean alarmOn = false;
 
 void setup()
 {
@@ -55,52 +82,99 @@ void setup()
   {
     RTC.getTime();
   }
+  pinMode(buzzer, OUTPUT);
+  pinMode(buttonMon, INPUT);
+  pinMode(buttonTue, INPUT);
+  pinMode(buttonWed, INPUT);
+  pinMode(buttonThu, INPUT);
+  pinMode(buttonFri, INPUT);
+  pinMode(buttonSat, INPUT);
+  pinMode(buttonSun, INPUT);
+  pinMode(lightSetting1, OUTPUT);
+  pinMode(lightSetting2, OUTPUT);
+  pinMode(lightSetting3, OUTPUT);
+  pinMode(lightControl, OUTPUT);
+
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(lightSetting1, LOW);
+  digitalWrite(lightSetting2, LOW);
+  digitalWrite(lightSetting3, LOW);
+  digitalWrite(lightControl, LOW);
 }
 
 void loop()
 {
   RTC.getTime();
 
-  // TODO: GET DAY OF WEEK v1
-  // TODO: CHECK MINUTE v1
-  // TODO: CHECK HOUR v1
-  // TODO: ALARMING v1
+  // Check Alarm
+  if (RTC.dow == 1)
+  {
+    checkAlarm(clocksMon, minMon, 0, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 2)
+  {
+    checkAlarm(clocksTue, minTue, 1, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 3)
+  {
+    checkAlarm(clocksWed, minWed, 2, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 4)
+  {
+    checkAlarm(clocksThu, minThu, 3, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 5)
+  {
+    checkAlarm(clocksFri, minFri, 4, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 6)
+  {
+    checkAlarm(clocksSat, minSat, 5, RTC.hour, RTC.minute, RTC.second);
+  }
+  else if (RTC.dow == 7)
+  {
+    checkAlarm(clocksSun, minSun, 6, RTC.hour, RTC.minute, RTC.second);
+  }
+
+  //Rise alarm
+  riseAlarm();
 
   // Add or delete alarm time
   if (stringComplete)
   {
     if (dayOfWeek == "MON")
     {
-      alarmSettings(clocksMon, 0);
+      alarmSettings(clocksMon, minMon, 0);
     }
     else if (dayOfWeek == "TUE")
     {
-      alarmSettings(clocksTue, 1);
+      alarmSettings(clocksTue, minTue, 1);
     }
     else if (dayOfWeek == "WED")
     {
-      alarmSettings(clocksWed, 2);
+      alarmSettings(clocksWed, minWed, 2);
     }
     else if (dayOfWeek == "THU")
     {
-      alarmSettings(clocksThu, 3);
+      alarmSettings(clocksThu, minThu, 3);
     }
     else if (dayOfWeek == "FRI")
     {
-      alarmSettings(clocksFri, 4);
+      alarmSettings(clocksFri, minFri, 4);
     }
     else if (dayOfWeek == "SAT")
     {
-      alarmSettings(clocksSat, 5);
+      alarmSettings(clocksSat, minSat, 5);
     }
     else if (dayOfWeek == "SUN")
     {
-      alarmSettings(clocksSun, 6);
+      alarmSettings(clocksSun, minSun, 6);
     }
 
     comMsg = "";
     dayOfWeek = "";
     clockData = "";
+    minData = "";
     stringComplete = false;
   }
 }
@@ -111,67 +185,295 @@ void serialEvent()
   while (Serial.available())
   {
     char inChar = (char)Serial.read();
-    clockData += inChar;
+    minData += inChar;
     if (inChar == '-')
     {
-      comMsg = clockData.substring(0, (clockData.length() - 1));
-      clockData = "";
+      comMsg = minData.substring(0, (minData.length() - 1));
+      minData = "";
     }
     if (inChar == ';')
     {
-      dayOfWeek = clockData.substring(0, (clockData.length() - 1));
-      clockData = "";
+      dayOfWeek = minData.substring(0, (minData.length() - 1));
+      minData = "";
+    }
+    if (inChar == ':')
+    {
+      clockData = minData.substring(0, (minData.length() - 1));
+      minData = "";
     }
     if (inChar == '!')
     {
       stringComplete = true;
-      clockData = clockData.substring(0, (clockData.length() - 1));
+      minData = minData.substring(0, (minData.length() - 1));
     }
   }
 }
 
 // Alarm settings
-void alarmSettings(String dayArray[], int dayNumber)
+void alarmSettings(String hourArray[], String minArray[], int dayNumber)
 {
   exist = false;
   // TODO: ARRAY LIMIT REACHED ERROR v2
   if (comMsg == "ADD")
   {
     if (numberOfAlarms[dayNumber] == 0) {
-      dayArray[0] = clockData;
+      hourArray[0] = clockData;
+      minArray[0] = minData;
     }
     else
     {
       for (int i = 0; i < numberOfAlarms[dayNumber]; i++)
       {
-        if (dayArray[numberOfAlarms[i]] == clockData )
+        if (hourArray[numberOfAlarms[i]] == clockData )
         {
-          exist = true;
-          // TODO: BUZZER FAIL v1
+          if (minArray[numberOfAlarms[i]] == minData)
+          {
+            exist = true;
+            digitalWrite(buzzer, LOW); // Buzzer
+            delay(1000);
+            digitalWrite(buzzer, HIGH); // Buzzer
+            delay(1000);
+            digitalWrite(buzzer, LOW); // Buzzer
+            delay(1000);
+          }
         }
       }
       if (!exist)
       {
-        dayArray[numberOfAlarms[dayNumber]] = clockData;
+        hourArray[numberOfAlarms[dayNumber]] = clockData;
+        minArray[numberOfAlarms[dayNumber]] = minData;
         numberOfAlarms[dayNumber] = numberOfAlarms[dayNumber] + 1;
       }
     }
   }
   else if (comMsg == "DEL")
   {
-    if (numberOfAlarms[dayNumber] != 0) {
+    if (numberOfAlarms[dayNumber] != 0)
+    {
       for (int i = 0; i < numberOfAlarms[dayNumber]; i++)
       {
-        if (dayArray[numberOfAlarms[i]] == clockData )
+        if (hourArray[numberOfAlarms[i]] == clockData )
         {
-          dayArray[numberOfAlarms[i]] = "";
-          numberOfAlarms[i] = numberOfAlarms[i] - 1;
-          break;
+          if (minArray[numberOfAlarms[i]] == minData )
+          {
+            hourArray[numberOfAlarms[i]] = "";
+            minArray[numberOfAlarms[i]] = "";
+            numberOfAlarms[i] = numberOfAlarms[i] - 1;
+            break;
+          }
         }
       }
-      // TODO: DELETE NOTTING ERROR v2
+      // TODO: DELETE NOTHING ERROR v2
     }
-    // TODO: NOTTING TO DELETE ERROR v2
+    // TODO: NOTHING TO DELETE ERROR v2
+  }
+}
+
+// Check Alarm
+void checkAlarm(String hourArray[], String minArray[], int dayNumber, int currentHour, int currentMin, int currentSec)
+{
+  if (numberOfAlarms[dayNumber] > 0)
+  {
+    for (int i = 0; i < numberOfAlarms[dayNumber]; i++)
+    {
+      if (minArray[i].toInt() == currentMin)
+      {
+        if (hourArray[i].toInt() == currentHour)
+        {
+          if (20 <= currentSec <= 40)
+          {
+            alarmStatus[dayNumber] = 1;
+            break;
+          }
+        }
+      }
+    }
+  }
+}
+
+void riseAlarm()
+{
+  if (alarmStatus[0] == 1)
+  {
+    // Give alarm on Monday
+    digitalWrite(lightSetting1, LOW); // Lights 0
+    digitalWrite(lightSetting2, LOW);
+    digitalWrite(lightSetting3, LOW);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonMon);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[0] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[1] == 1)
+  {
+    // Give alarm on Tuesday
+    digitalWrite(lightSetting1, HIGH); // Lights 1
+    digitalWrite(lightSetting2, LOW);
+    digitalWrite(lightSetting3, LOW);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonTue);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[1] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[2] == 1)
+  {
+    // Give alarm on Wednesday
+    digitalWrite(lightSetting1, LOW); // Lights 2
+    digitalWrite(lightSetting2, HIGH);
+    digitalWrite(lightSetting3, LOW);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonWed);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[2] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[3] == 1)
+  {
+    // Give alarm on Thursday
+    digitalWrite(lightSetting1, HIGH); // Lights 3
+    digitalWrite(lightSetting2, HIGH);
+    digitalWrite(lightSetting3, LOW);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonThu);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[3] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[4] == 1)
+  {
+    // Give alarm on Friday
+    digitalWrite(lightSetting1, LOW); // Lights 4
+    digitalWrite(lightSetting2, LOW);
+    digitalWrite(lightSetting3, HIGH);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonFri);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[4] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[5] == 1)
+  {
+    // Give alarm on Saturday
+    digitalWrite(lightSetting1, HIGH); // Lights 5
+    digitalWrite(lightSetting2, LOW);
+    digitalWrite(lightSetting3, HIGH);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonSat);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[5] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
+  }
+  else if (alarmStatus[6] == 1)
+  {
+    // Give alarm on Sunday
+    digitalWrite(lightSetting1, LOW); // Lights 6
+    digitalWrite(lightSetting2, HIGH);
+    digitalWrite(lightSetting3, HIGH);
+
+    while (true)
+    {
+      buttonState = digitalRead(buttonSun);
+      if (buttonState == HIGH)
+      {
+        // Box Opened Close Alarm
+        alarmStatus[0] == 0;
+        digitalWrite(buzzer, HIGH); // Buzzer
+        digitalWrite(lightControl, LOW); // Led
+        delay(10000);
+        break;
+      }
+      digitalWrite(buzzer, LOW); // Buzzer
+      digitalWrite(lightControl, HIGH); // Led
+      delay(delayPeriod);
+      digitalWrite(buzzer, HIGH); // Buzzer
+      digitalWrite(lightControl, LOW); // Led
+    }
   }
 }
 
